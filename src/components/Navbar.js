9eb2a6becf9602d48c33fa813f0d16ead105421d
gijsvandenbeuken <g.vandenbeuken@student.fontys.nl>
2024-06-27 12:53:33 +0200

    Added navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 export const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navbar-container">
-        <div className="navbar-logo"></div>
+        <div className="navbar-logo">Logo here</div>
         <div className="navbar-links">
           <Link className="navbar-link" to="/portfolio">
             Portfolio
           </Link>
           <Link className="navbar-link" to="/about">
-            About
+            Over mij
           </Link>
-          <Link className="navbar-link" to="/contact">
+          <Link className="navbar-link" to="/about">
+            Archives
+          </Link>
+        </div>
+        <div className="navbar-contact">
+          <Link className="navbar-btn" to="/contact">
             Contact
           </Link>
         </div>
